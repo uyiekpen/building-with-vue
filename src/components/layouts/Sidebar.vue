be0ae7d99eadb2661f68f8015cc6efa0aecb6a1c
Osazie <uyiekpenelizabeth@gmail.com>
2024-06-06 13:59:23 +0100

    dashboard design

--- src/components/layouts/Sidebar.vue
+++ src/components/layouts/Sidebar.vue
@@ -1,27 +1,39 @@
 <template>
-    <div :class="['absolute left-0 top-0 z-9999 flex h-screen md:w-[260px] w-72.5  flex-col overflow-y-hidden md:bg-[#F3F4F6] bg-white duration-300 ease-linear lg:static lg:translate-x-0 border-r-2 p-4 transition-transform', { '-translate-x-full': !isOpen }]">
-      <ul>
-        <li v-for="item in menuItems" :key="item.name" class="mb-4">
-          <router-link :to="item.path" class="flex items-center p-2 hover:bg-gray-700 rounded">
-            <font-awesome-icon :icon="item.icon" class="mr-2" />
-            <span class="hidden md:inline">{{ item.name }}</span>
-          </router-link>
-        </li>
-      </ul>
+  <div
+    :class="['absolute left-0 top-0 z-9999 flex h-screen md:w-[260px] w-72.5  flex-col overflow-y-hidden md:bg-[#F3F4F6] bg-white duration-300 ease-linear lg:static lg:translate-x-0 border-r-2 p-4 transition-transform', { '-translate-x-full': !isOpen }]"
+  >
+    <div>
+      <img :src="logo" />
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref, defineProps } from 'vue';
-  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-  import { faHome, faUsers, faCog } from '@fortawesome/free-solid-svg-icons';
-  
-  const props = defineProps<{ isOpen: boolean }>();
-  
-  const menuItems = ref([
-    { name: 'Home', path: '/', icon: faHome },
-    { name: 'Users', path: '/users', icon: faUsers },
-    { name: 'Settings', path: '/settings', icon: faCog },
+    <ul>
+      <li v-for="item in menuItems" :key="item.name" class="mb-4">
+        <router-link
+          :to="item.path"
+          class="flex items-center p-2 hover:bg-[#E2E4E7] rounded active:bg-[#E2E4E7]"
+        >
+          <font-awesome-icon :icon="item.icon" class="mr-2" />
+          <span class="hidden md:inline">{{ item.name }}</span>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { faCog, faHome, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { defineProps, ref } from 'vue';
+import logo from "../../assets/logo-light-full.png";
+
+const props = defineProps<{ isOpen: boolean }>();
+
+const menuItems = ref([
+    { name: 'Dashboard', path: '/dashboard', icon: faHome },
+    { name: 'Product List', path: '/users', icon: faUsers },
+    { name: 'Product Edit', path: '/settings', icon: faCog },
+    { name: 'New Product ', path: '/settings', icon: faCog },
+    { name: 'Order list', path: '/settings', icon: faCog },
+    { name: 'Ordet details', path: '/settings', icon: faCog },
+
   ]);
-  </script>
-  
+</script>
